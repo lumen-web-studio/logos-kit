f74753adeac6b9f5fa46a919c178eb0476d45d54
Gruschka <gegruschka@gmail.com>
2026-03-30 17:10:35 -0300

    Add GA4

---
+++ GoogleAnalytics.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Script from "next/script";
+import type { ReactElement } from "react";
+
+type Props = {
+  measurementId: string | undefined;
+};
+
+export default function GoogleAnalytics({
+  measurementId,
+}: Props): ReactElement | null {
+  if (!measurementId) return null;
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${measurementId}');
+        `}
+      </Script>
+    </>
+  );
+}
