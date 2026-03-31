f85917b2f11c80a8f5c7eaf11ff7329c8172b8d9
Gruschka <gegruschka@gmail.com>
2026-03-31 11:48:59 -0300

    feat: run GA in web worker via Partytown for better performance

--- GoogleAnalytics.tsx
+++ GoogleAnalytics.tsx
@@ -16,15 +16,10 @@ export default function GoogleAnalytics({
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
-        strategy="afterInteractive"
+        strategy="worker"
       />
-      <Script id="ga-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${measurementId}');
-        `}
+      <Script id="ga-init" strategy="worker">
+        {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${measurementId}');`}
       </Script>
     </>
   );
